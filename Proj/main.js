fcd96cccd8980e19f5d3fdd71f52f84e623e987c
tommygomez25 <maysan54321@gmail.com>
2024-04-16 19:23:58 +0100

    User bucket

--- Proj/main.js
+++ Proj/main.js
@@ -1,8 +1,8 @@
 var Riak = require('basho-riak-client');
+var async = require('async');
+var fs = require('fs');
 
-var nodes = [
-    'localhost:8087'
-];
+var nodes = ['localhost:8087'];
 
 var client = new Riak.Client(nodes, function (err, c) {
     if (err) {
@@ -12,90 +12,36 @@ var client = new Riak.Client(nodes, function (err, c) {
     }
 });
 
-client.ping(function (err, rslt) {
+fs.readFile('./data/users.json', 'utf8', function (err, data) {
     if (err) {
         throw new Error(err);
     } else {
-        console.log(rslt);
-    }
-});
+        var users = JSON.parse(data);
 
-var async = require('async');
+        console.log('Data loaded from file: ' + users.length + ' users');
 
-var people = [
-    {
-        emailAddress: "bashoman@riak.com",
-        firstName: "Riak",
-        lastName: "Man"
-    },
-    {
-        emailAddress: "johndoe@gmail.com",
-        firstName: "John",
-        lastName: "Doe"
-    }
-];
-
-var storeFuncs = [];
-people.forEach(function (person) {
-    // Create functions to execute in parallel to store people
-    storeFuncs.push(function (async_cb) {
-        client.storeValue({
-                bucket: 'contributors',
-                key: person.emailAddress,
-                value: person
-            },
-            function(err, rslt) {
-                async_cb(err, rslt);
-            }
-        );
-    });
-});
-
-async.parallel(storeFuncs, function (err, rslts) {
-    if (err) {
-        throw new Error(err);
-    }
-});
-
-var logger = require('winston');
-
-client.fetchValue({ bucket: 'contributors', key: 'bashoman@riak.com', convertToJs: true },
-    function (err, rslt) {
-        if (err) {
-            throw new Error(err);
-        } else {
-            var riakObj = rslt.values.shift();
-            var bashoman = riakObj.value;
-            logger.info("I found %s in 'contributors'", bashoman.emailAddress);
-            bashoman.FirstName = "Riak";
-            riakObj.setValue(bashoman);
-            client.storeValue({ value: riakObj }, function (err, rslt) {
-                if (err) {
-                    throw new Error(err);
-                }
-            });
-        
-        }
-    }
-);
-
-// print first names of all contributors
-client.listKeys({ bucket: 'contributors' }, function (err, rslt) {
-    if (err) {
-        throw new Error(err);
-    } else {
-        rslt.keys.forEach(function (key) {
-            client.fetchValue({ bucket: 'contributors', key: key, convertToJs: true },
-                function (err, rslt) {
-                    if (err) {
-                        throw new Error(err);
-                    } else {
-                        var riakObj = rslt.values.shift();
-                        var person = riakObj.value;
-                        logger.info("Contributor: %s", person.firstName);
+        var storeFuncs = [];
+        users.forEach(function (user) {
+            // create functions execute in parallel
+            storeFuncs.push(function (async_cb) {
+                client.storeValue({
+                        bucket: 'user',
+                        key: user.username,
+                        value: user
+                    },
+                    function (err, rslt) {
+                        async_cb(err, rslt);
                     }
-                }
-            );
+                );
+            });
+        });
+
+        async.parallel(storeFuncs, function (err, rslts) {
+            if (err) {
+                throw new Error(err);
+            } else {
+                console.log('Data saved in bucket User');
+            }
         });
     }
 });
